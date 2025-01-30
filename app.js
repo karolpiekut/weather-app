bd8b4b2ef6534c3809f53bf342107525ba89062a
kpiekut <karol.piekut@pm.me>
2025-01-30 21:03:37 +0000

    add svgs

--- app.js
+++ app.js
@@ -17,26 +17,65 @@ async function getWeather() {
     return await response.json();
 }
 
-const moonSVG = document.createElement('img');
-moonSVG.src = 'icons/clear-night.svg';
-const partlyCloudySVG = document.createElement('img');
-partlyCloudySVG.src = 'icons/partly-cloudy-day.svg';
-const snowSVG = document.createElement('img');
-snowSVG.src = 'icons/snow.svg';
-const sunnySVG = document.createElement('img');
-sunnySVG.src = 'icons/clear-day.svg';
-const rainSVG = document.createElement('img');
-rainSVG.src = 'icons/rain.svg';
-const thunderstormSVG = document.createElement('img');
-thunderstormSVG.src = 'icons/thunder.svg';
+const blowingSnowSVG = document.createElement('img');
+blowingSnowSVG.src = 'weather-icons/blowing-snow.svg';
+const clearNightSVG = document.createElement('img');
+clearNightSVG.src = 'weather-icons/clear-night.svg';
 const cloudySVG = document.createElement('img');
-cloudySVG.src = 'icons/cloudy.svg';
+cloudySVG.src = 'weather-icons/cloudy.svg';
+const drizzleSVG = document.createElement('img');
+drizzleSVG.src = 'weather-icons/drizzle.svg';
+const duststormSVG = document.createElement('img');
+duststormSVG.src = 'weather-icons/duststorm.svg';
+const fogSVG = document.createElement('img');
+fogSVG.src = 'weather-icons/fog.svg';
+const hailDaySVG = document.createElement('img');
+hailDaySVG.src = 'weather-icons/hail-day.svg';
+const hailNightSVG = document.createElement('img');
+hailNightSVG.src = 'weather-icons/hail-night.svg';
+const hazeSVG = document.createElement('img');
+hazeSVG.src = 'weather-icons/haze.svg';
+const partlyCloudySVG = document.createElement('img');
+partlyCloudySVG.src = 'weather-icons/partly-cloudy.svg';
+const rainSVG = document.createElement('img');
+rainSVG.src = 'weather-icons/rain.svg';
+const rainShowersDaySVG = document.createElement('img');
+rainShowersDaySVG.src = 'weather-icons/rain-showers-day.svg';
+const rainShowersNightSVG = document.createElement('img');
+rainShowersNightSVG.src = 'weather-icons/rain-showers-night.svg';
+const rainSnowSVG = document.createElement('img');
+rainSnowSVG.src = 'weather-icons/rain-snow.svg';
+const snowSVG = document.createElement('img');
+snowSVG.src = 'weather-icons/snow.svg';
+const snowShowerDaySVG = document.createElement('img');
+snowShowerDaySVG.src = 'weather-icons/snow-shower-day.svg';
+const snowShowerNightSVG = document.createElement('img');
+snowShowerNightSVG.src = 'weather-icons/snow-shower-night.svg';
+const snowflakeSVG = document.createElement('img');
+snowflakeSVG.src = 'weather-icons/snowflake.svg';
+const squallsSVG = document.createElement('img');
+squallsSVG.src = 'weather-icons/squalls.svg';
+const strongWindSVG = document.createElement('img');
+strongWindSVG.src = 'weather-icons/strong-wind.svg';
+const sunnySVG = document.createElement('img');
+sunnySVG.src = 'weather-icons/sunny.svg';
+const sunnyHighSVG = document.createElement('img');
+sunnyHighSVG.src = 'weather-icons/sunny-high.svg';
+const sunnyLowSVG = document.createElement('img');
+sunnyLowSVG.src = 'weather-icons/sunny-low.svg';
+const thunderstormSVG = document.createElement('img');
+thunderstormSVG.src = 'weather-icons/thunderstorm.svg';
+
 
 const locationOutput = document.querySelector('#location');
 const temperature = document.querySelector('#temperature');
 const cButton = document.querySelector('#changeToCelsius');
 const fButton = document.querySelector('#changeToFahrenheit');
+const weatherIcon = document.querySelector('#weatherIcon');
 
+weatherIcon.src = sunnySVG.src;
+
+console.log(weatherIcon);
 
 let locationVar = "New York";  //zbędne, zastąp text content w środku funkcji na dole (async)
 let temperatureVar = 38.7;
@@ -79,10 +118,10 @@ function amendToCelsius(val) {
 
     // addWeatherData(searchResult);
     // addWeatherData(temperature);
-    //temperatureVar =  currentPlace.currentConditions.temp
+    //temperatureVar =  currentPlace.currentConditions.tempi
     temperature.innerText = currentPlace.currentConditions.temp + "F";
     locationOutput.innerText = currentPlace.address;
-    //console.log(currentPlace.currentConditions.conditions);
+    console.log(currentPlace.currentConditions.conditions);
 
     cButton.addEventListener('click', function() {
       amendToCelsius(currentPlace.currentConditions.temp);
@@ -93,11 +132,3 @@ function amendToCelsius(val) {
         temperature.innerText = currentPlace.currentConditions.temp + "F";
     })
 })()
-
-
-
-
-
-
-
-
