61775edaf73457f623bd1bb858fab959ced43fdf
kpiekut <karol.piekut@pm.me>
2025-03-06 16:25:54 +0000

    test

--- app.js
+++ app.js
@@ -11,6 +11,7 @@
 
 async function getWeather() {
     const response = await fetch("./newYork.json",
+    //const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Walbrzych?unitGroup=us&key=LD4DKRU5NCMMDQM365J4C6429&contentType=json",
         {
             mode: "cors",
         });
@@ -24,8 +25,6 @@ const fButton = document.querySelector('#changeToFahrenheit');
 const weatherIcon = document.querySelector('#weatherIcon');
 const searchBox = document.querySelector('#searchBox');
 const submitButton = document.querySelector('#submitBtn');
-
-console.log(submitButton);
 
 function createWeatherIcon(conditionName) {
     const iconSVG = document.createElement('img');
@@ -43,12 +42,20 @@ function amendToCelsius(val) {
 
 (async () => {
     let currentPlace = (await getWeather())
+    let defaultLocation = "New York";
     temperature.innerText = currentPlace.currentConditions.temp + "F";
     locationOutput.innerText = currentPlace.address;
     let createWeatherIconTemp = createWeatherIcon(currentPlace.currentConditions.icon);
     weatherIcon.src = createWeatherIconTemp.src;
     weatherIcon.alt = currentPlace.currentConditions.icon;
-    console.log(searchBox.value); 
+
+    submitButton.addEventListener('click', function() {
+        if (searchBox.value === "") {
+            alert("please input a search value");
+        } else {
+            alert(`I am going to search for that: "${searchBox.value}"`);
+        }
+    });
 
     cButton.addEventListener('click', function() {
       amendToCelsius(currentPlace.currentConditions.temp);
